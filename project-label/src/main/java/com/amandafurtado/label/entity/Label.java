28540978cc9033c17a01ae00ba68a3521c58d4d2
Amanda approved! <vieira.a.amanda@gmail.com>
2022-09-20 14:44:25 +0100

    adding column notation

--- project-label/src/main/java/com/amandafurtado/label/entity/Label.java
+++ project-label/src/main/java/com/amandafurtado/label/entity/Label.java
@@ -1,13 +1,17 @@
 package com.amandafurtado.label.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 @Entity
-public class Label extends PanacheEntity{
-	
+public class Label extends PanacheEntity {
+
+	@Column
 	private String labelCode;
+
+	@Column
 	private String labelDescription;
 
 }
