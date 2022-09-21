0be2f10b8196380107b9d6a40af57cb169ae8635
Amanda approved! <vieira.a.amanda@gmail.com>
2022-09-21 10:11:35 +0100

    adding http methods findbyid, update and remove

--- project-label/src/main/java/com/amandafurtado/label/resource/LabelResource.java
+++ project-label/src/main/java/com/amandafurtado/label/resource/LabelResource.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -22,7 +26,6 @@ public class LabelResource {
 	    LabelService labelService;
 
 	    @GET
-	    
 	    public Response ListLabels() {
 	    	
 	        List<Label> labels = labelService.listLabel();
@@ -30,11 +33,41 @@ public class LabelResource {
 	        return Response.ok(labels).build();
 	    }
 	    
+	    @GET
+		@Path("{id}")
+		public Response findById(@PathParam("id") Long id) {
+
+			Label label = labelService.findById(id);
+			
+			return Response.ok(label).status(200).build();
+		
+	    }
+	    
+	    @POST
 	    public Response save(Label label) {
 	    	
 	    	Label labels = labelService.save(label);
 	    	
 	    	return Response.ok(labels).status(201).build();
+	    }
+	    
+	    @PUT
+	    @Path("{id}")
+	    public Response update(@PathParam("id") Long id, Label label) {
+	    	
+	    	labelService.update(id, label);
+	    	
+	    	return Response.status(204).build();
+	    
+	    }
+	    
+	    @DELETE
+	    @Path("{id}")
+	    public Response remove(@PathParam("id") Long id) {
+	    	
+	    	labelService.remove(id);
+	    	
+	    	return Response.status(204).build();
 	    }
 	        
 
