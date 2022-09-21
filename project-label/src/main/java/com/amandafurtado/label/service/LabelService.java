0a4356388cec2184c2e2e1d90766e27768d2f602
Amanda approved! <vieira.a.amanda@gmail.com>
2022-09-21 10:09:02 +0100

    adding methods findbyid, update and remove

--- project-label/src/main/java/com/amandafurtado/label/service/LabelService.java
+++ project-label/src/main/java/com/amandafurtado/label/service/LabelService.java
@@ -1,6 +1,7 @@
 package com.amandafurtado.label.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
@@ -16,6 +17,13 @@ public class LabelService {
 	}
 	
 	@Transactional
+	public Label findById(Long id) {
+
+		Label label = findById(id);
+		return label;
+	}
+	
+	@Transactional
 	public Label save(Label label) {
 		Label labels = new Label();
 		labels.setLabelCode(labels.getLabelCode());
@@ -24,6 +32,38 @@ public class LabelService {
 		labels.persist();
 		return labels;
 	}
+	
+	@Transactional
+	public void update(Long id, Label label) {
+		
+		Label labels = new Label();
+		
+		Optional<Label> labelOp = label.findByIdOptional(id);
+		
+		if (labelOp.isEmpty()) {
+			throw new NullPointerException("Label not found");
+		}
+		labels = labelOp.get();
+		labels.setLabelCode(label.getLabelCode());
+		labels.setLabelDescription(label.getLabelDescription());
+		
+		labels.persist();
+	}
+	
+	@Transactional
+	public void remove(Long id) {
+		
+		Optional<Label> labelOp = Label.findByIdOptional(id);
+		
+		if(labelOp.isEmpty()) {
+			throw new NullPointerException("Label not found");
+		}
+		
+		Label label = labelOp.get();
+		
+		label.delete();
+	}
+		
 	
 	
 
