332270e1bade2ae99ed88003b0e55abee00ae9d6
Amanda approved! <vieira.a.amanda@gmail.com>
2022-09-21 10:12:42 +0100

    trying to solve a configuration problem

--- project-label/src/main/java/com/amandafurtado/label/entity/Label.java
+++ project-label/src/main/java/com/amandafurtado/label/entity/Label.java
@@ -1,23 +1,28 @@
 package com.amandafurtado.label.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 @Entity
-//@Table(name = "labels")
+//@Table(name = "labels2")
 public class Label extends PanacheEntity {
 
+	@NotBlank(message = "Code may not be blank")
 //	@Column(name = "Label_Code")
 	private String labelCode;
 
+	@NotBlank(message = "Description may not be blank")
 //	@Column(name = "Label_Description")
 	private String labelDescription;
 	
 	public Long getId() {
 		return id;
+	}
+	
+	public void setLabelCode(Long id) {
+		this.id = id;
 	}
 
 	public String getLabelCode() {
